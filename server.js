57b4f0d099493fda7d18f263690071fa24941a74
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-10-12 23:08:17 -0400

    middleware for json, books route

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    
+   console.log("here") 
 })
+
+
+app.get('/books/', (req, res) => {
+   console.log("here") 
+})
+
 app.listen(port)
